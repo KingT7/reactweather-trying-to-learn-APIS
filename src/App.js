33e02ade99eeef56e1dabf46f6f81bd364e83095
Nicholas H <NicholasH7@hotmail.com>
2022-05-31 20:51:40 -0400

    trying to learn axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from "react"; 
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [data, setData] = useState({});
+
+const[location, setLocation] = useState('');
+
+const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid={f6d10ca5872c8117e03769b0d64a279f}`
+    
+const search = (e) => {
+    if (e.key === 'Enter') {
+        axios.get(url).then((res) => {
+            console.log(res.data)
+            e.preventDefault()
+            
+        })
+    }
 }
 
-export default App;
+    return( 
+<div className="mainContent">
+  
+   <form>
+    <input className="Input" type="text" placeholder="Search a location..."
+        onChange={ e => setLocation(e.target.value)}
+        onKeyPress= {search}
+        
+    />
+    <br/>
+      <button className="searchBtn" type="Sumbit"> Sumbit </button>  
+</form>
+
+<p> {data.name}</p>
+
+
+
+
+
+
+  </div>
+
+);};
+
+    export default App; 
